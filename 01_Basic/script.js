0001a206b2bc62ec0c13e7ee232c063b627999db
devanshkhatri12 <dkkhatri941@gmail.com>
2024-12-12 09:42:50 +0530

    Revision

--- 01_Basic/script.js
+++ 01_Basic/script.js
@@ -1,10 +1,10 @@
 // gsap.to("#box", {
 //     x:1000,
 //     y:250,
-//     duration: 2,
+//     duration: 5,
 //     delay: 1,
 //     borderRadius: "50%",
-//     scale: 1,
+//     scale: 0,       // scale 1 means originalsize or 2 means biggeror0 means smaller
 //     repeat: -1,
 //     yoyo: true
 // })
@@ -19,30 +19,30 @@
 // })
 
 
-var tl = gsap.timeline()
+// var tl = gsap.timeline()
 
-tl.from("h2", {
-    y: -30,
-    duration: 0.5,
-    delay: 1,
-    opacity: 0
-})
+// tl.from("h2", {
+//     y: -30,
+//     duration: 0.5,
+//     delay: 1,
+//     opacity: 0
+// })
 
-tl.from("h4", {
-    y: -30,
-    duration: 0.5,
-    delay: 0.5,
-    opacity: 0,
-    stagger: 0.5
-})
+// tl.from("h4", {
+//     y: -30,          // upr se niche aayega
+//     duration: 0.5,
+//     delay: 0.5,
+//     opacity: 0,
+//     stagger: 5       // ek-ek krke chlege kitne sec. baad appear honge
+// })
 
-tl.from("h1", {
-    y: 30,
-    duration: 0.5,
-    delay: 0.5,
-    opacity: 0,
-    scale: 0.2
-})
+// tl.from("h1", {
+//     y: 30,            // nivhe se upr aayega
+//     duration: 0.5,
+//     delay: 0.5,
+//     opacity: 0,
+//     scale: 1
+// })
 
 
 gsap.from(".page1 .box",{
@@ -57,12 +57,12 @@ gsap.from(".page2 .box",{
     duration: 1,
     delay: 1,
     rotate: 180,
-    // scrollTrigger: ".page2 .box"             method -1
-    scrollTrigger: {
-        trigger: ".page2 .box",
-        scroller: "body",
-        markers: true,
-        start: "top 60%"
-    }
+    scrollTrigger: ".page2 .box"            // method -1
+    // scrollTrigger: {
+    //     trigger: ".page2 .box",
+    //     scroller: "body",
+    //     markers: true,
+    //     start: "top 60%"
+    // }
 })
 
